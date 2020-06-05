e8b173ffcb23c818333e7a130340ac393013afc0
Zamir <ZamirAmiri@github.com>
2020-06-05 22:35:53 +0200

    Hello

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Product from './Product';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class Home extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      winkelmandje:[],
+      producten:[
+        {itemId:1,name:'footbal 1', prize:10},
+        {itemId:1,name:'footbal 2', prize:10},
+        {itemId:1,name:'footbal 3', prize:10},
+        {itemId:1,name:'footbal 4', prize:10},
+        {itemId:1,name:'footbal 5', prize:10},
+      ],
+    }
+  }
+
+  addToWinkelMandje = (name,prize,itemId)=>{
+    const item = {
+      naam:name,
+      prijs:prize,
+      itemId:itemId
+    };
+    var winkelmand = [];
+    winkelmand = winkelmand.concat(this.state.winkelmandje);
+    winkelmand.push(item);
+    this.setState({winkelmandje:winkelmand});
+    console.log(winkelmand);
+  }
+
+  login = (email,pass)=>{
+    var response = fetch('server.domainnaam.nl/Login/email='+email+'&pass='+pass);
+    response.JSON(data => {
+      console.log(data);
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+
+        <div style={{ display:'flex', alignItems:'center', justifyContent:'center', flexDirection:'row'}}>
+          {this.state.producten.map(
+            (product)=>{
+              return(
+                <Product/>
+              );
+            }
+          )}
+        </div>
+        <br/>
+        <button
+          value='Show Cart'
+          
+          onCLick={this.showCart}
+        >Show Cart</button>
+        <br/>
+        <br/>
+        <br/>
+        {this.state.winkelmandje.map(
+          (item)=>{
+            return(
+              <div>
+                {item.naam}
+                <br/>
+                {item.prijs}
+                <br/>
+                {item.itemId}
+                <br/>
+              </div>
+            );
+          }
+        )}
+      </div>
+    );
+
+  }
 }
-
-export default App;
